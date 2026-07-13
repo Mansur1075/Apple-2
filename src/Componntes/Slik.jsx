7cd7fc80501192b47016fccaa98df0a5c24c712c
Mansur1075 <Mansur1075@users.noreply.github.com>
2026-07-13 23:25:52 +0600

    mansur

--- src/Componntes/Slik.jsx
+++ src/Componntes/Slik.jsx
@@ -1,14 +1,67 @@
 import React from 'react'
 import Container from './Container'
+import rto from "../assets/rto.png"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Slik = () => {
-  return (
-    <div>
-      <Container>
-        <h4>lfcj</h4>
-      </Container>
-    </div>
-  )
+    return (
+        <div className="bg-[#000000] py-16 w-full overflow-hidden">
+            <div className="w-full mx-auto">
+                <Swiper
+                    modules={[Autoplay, Pagination]}
+                    spaceBetween={16}
+                    slidesPerView={1.2}
+                    centeredSlides={true}
+                    loop={true}
+                    loopedSlides={2}
+                    autoplay={{
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{ clickable: true }}
+                    breakpoints={{
+                        768: {
+                            slidesPerView: 1.5,
+                            spaceBetween: 24,
+                        },
+                        1024: {
+                            slidesPerView: 1.62,
+                            spaceBetween: 32,
+                        }
+                    }}
+                    className="apple-swiper w-full pb-12"
+                >
+                    <SwiperSlide className="h-auto aspect-video">
+                        <img src={rto} alt="Slide 1" className='w-full h-full object-cover rounded-2xl md:rounded-3xl block shadow-2xl' />
+                    </SwiperSlide>
+                    
+                    <SwiperSlide className="h-auto aspect-video">
+                        <img src={rto} alt="Slide 2" className='w-full h-full object-cover rounded-2xl md:rounded-3xl block shadow-2xl' />
+                    </SwiperSlide>
+                    
+                    <SwiperSlide className="h-auto aspect-video">
+                        <img src={rto} alt="Slide 3" className='w-full h-full object-cover rounded-2xl md:rounded-3xl block shadow-2xl' />
+                    </SwiperSlide>
+
+                    <SwiperSlide className="h-auto aspect-video">
+                        <img src={rto} alt="Slide 4" className='w-full h-full object-cover rounded-2xl md:rounded-3xl block shadow-2xl' />
+                    </SwiperSlide>
+
+                    <SwiperSlide className="h-auto aspect-video">
+                        <img src={rto} alt="Slide 5" className='w-full h-full object-cover rounded-2xl md:rounded-3xl block shadow-2xl' />
+                    </SwiperSlide>
+
+                    <SwiperSlide className="h-auto aspect-video">
+                        <img src={rto} alt="Slide 6" className='w-full h-full object-cover rounded-2xl md:rounded-3xl block shadow-2xl' />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    )
 }
 
 export default Slik
