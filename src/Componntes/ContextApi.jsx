a8cbb843cf8f346301da286bf041f30e1bb00443
Mansur1075 <Mansur1075@users.noreply.github.com>
2026-07-15 19:21:54 +0600

    ma

---
+++ src/Componntes/ContextApi.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const ContextApi = () => {
+  return (
+    <div>
+        <Container>
+            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, eveniet?</h5>
+        </Container>
+        </div>
+  )
+}
+
+export default ContextApi
