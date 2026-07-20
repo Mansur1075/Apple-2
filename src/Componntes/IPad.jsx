5dca80b7b83498bd4867f0707b1ef92a81fb1ec2
Mansur1075 <Mansur1075@users.noreply.github.com>
2026-07-20 20:41:18 +0600

    mansur

--- src/Componntes/IPad.jsx
+++ src/Componntes/IPad.jsx
@@ -5,14 +5,14 @@ const IPad = () => {
   return (
     <>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-3 p-3 bg-white font-sans">
-
-        <div className="h-125 md:h-145 w-full relative overflow-hidden flex flex-col items-center justify-start pt-12 md:pt-16 px-4">
-
+        <div className="h-125 md:h-145 w-full relative overflow-hidden flex flex-col items-center justify-start pt-12 md:pt-16 px-4 group">
           <img
             src={ipad2}
             alt="iPad Background"
-            className="absolute inset-0 w-full h-full object-cover z-0 hover:scale-105 ease-in-out duration-300 "
+            className="absolute inset-0 w-full h-full object-cover z-0 transition-transform ease-in-out duration-700 group-hover:scale-110"
           />
+          <span className="absolute inset-y-0 -left-full block w-1/2 h-full bg-linear-to-r from-transparent via-black/20 to-transparent opacity-70 skew-x-[-25deg] transition-all ease-in-out duration-1000 group-hover:left-[150%] z-0 pointer-events-none"></span>
+
           <div className="text-center z-10">
             <h2 className="text-3xl md:text-4xl font-bold tracking-tight text-black">iPad</h2>
             <p className="text-base md:text-lg mt-1 text-[#1d1d1f]">Lovable. Drawable. Magical.</p>
@@ -22,12 +22,13 @@ const IPad = () => {
             </div>
           </div>
         </div>
-        <div className="h-125 md:h-145 w-full relative overflow-hidden flex flex-col items-center justify-start pt-12 md:pt-16 px-4">
+        <div className="h-125 md:h-145 w-full relative overflow-hidden flex flex-col items-center justify-start pt-12 md:pt-16 px-4 group">
           <img
             src={ipad1}
             alt="MacBook Background"
-            className="absolute inset-0 w-full h-full object-cover z-0 hover:scale-105 ease-in-out duration-300"
+            className="absolute inset-0 w-full h-full object-cover z-0 transition-transform ease-in-out duration-700 group-hover:scale-110"
           />
+          <span className="absolute inset-y-0 -left-full block w-1/2 h-full bg-linear-to-r from-transparent via-white to-transparent opacity-30 skew-x-[-25deg] transition-all ease-in-out duration-1000 group-hover:left-[150%] z-0 pointer-events-none"></span>
           <div className="text-center z-10">
             <h2 className="text-3xl md:text-4xl font-bold tracking-tight text-white">MacBook Pro</h2>
             <p className="text-base md:text-lg mt-1 text-[#86868b]">Supercharged by M2 Pro and M2 Max.</p>
